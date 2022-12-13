53be1894fbb55451f1a8b95949be5778a8e32e6a
Cláudio Más <claudio.mas@gmail.com>
2022-12-12 23:55:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ with col1:
 with col2:
     #st.image("https://as1.ftcdn.net/jpg/03/07/43/75/240_F_307437510_x6kug0WyeBJQjzhjVs3jTbIQkpJBDPP1.jpg", width=300)
     col2.markdown("""<p><img style="float: right;" src="https://as1.ftcdn.net/jpg/03/07/43/75/240_F_307437510_x6kug0WyeBJQjzhjVs3jTbIQkpJBDPP1.jpg" alt="" width="240" height="240" /></p>""", unsafe_allow_html=True)
-    col2.st.button("Nova ficha", on_click=clear_text)
+    st.button("Nova ficha", on_click=clear_text)
     

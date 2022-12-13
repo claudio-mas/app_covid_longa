83f7b07e2e70f16f950d664380e8fc4475f25091
Cláudio Más <claudio.mas@gmail.com>
2022-12-13 18:51:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as se
 import warnings
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
